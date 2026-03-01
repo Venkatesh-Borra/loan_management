aa2963dc4ae326301617432f3ca47a1d7f911265
Venkatesh Borra <129498557+Venkatesh-Borra@users.noreply.github.com>
2026-03-01 23:56:38 +0530

    Create LoanRepository interface for Spring Data JPA

---
+++ src/main/java/com/loanmanagement/repository/LoanRepository.java
@@ -0,0 +1,7 @@
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LoanRepository extends JpaRepository<Loan, Long> {
+    // Custom query methods (if any) can be defined here
+}
